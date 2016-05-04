e36e20c1727e03928949c68bc8b356c596342db5
Aaron Roemig <aaronroemig@gmail.com>
2016-05-04 17:32:44 -0500

    a

--- scripts/example.js
+++ scripts/example.js
@@ -30,5 +30,28 @@ module.exports = function(robot) {
 	})
 
 
+var d = new Date();
+var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+
+if 
+	robot.hear(/Saturday/i, function(response) {
+		response.send("PARTY!")
+		}) { 
+} else if robot.hear(/Sunday/i, function(response) {
+		response.send("SundayFunDay")
+}) else 
+	robot.hear(/Monday, Tuesday, Wednesday, Thursday/i, function(response) { 
+			response.send("FML")
+})
+
 
 }
